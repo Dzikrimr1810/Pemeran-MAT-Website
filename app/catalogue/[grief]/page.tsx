164f7ca2482e77715697f5adce44abf7f4b3d459
unknown <dzikri.mr1810@gmail.com>
2024-12-19 10:20:46 +0700

    add

--- app/catalogue/[grief]/page.tsx
+++ app/catalogue/[grief]/page.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import Categories from '@/app/components/categories';
 import ProjectGrief from '@/app/components/projectGrief';
 
+// Menambahkan fungsi generateStaticParams
+export async function generateStaticParams() {
+  // Daftar kategori grief yang bisa diakses oleh rute dinamis
+  const griefCategories = ['Denial', 'Anger', 'Bargaining', 'Depression', 'Acceptance'];
+
+  return griefCategories.map(grief => ({
+    grief, // params yang akan diproses oleh halaman ini
+  }));
+}
+
 // Komponen Grief dengan async di dalamnya
 const Grief = async ({ params }: { params: { grief: string } }) => {
-  // Anda bisa menggunakan fetch di sini untuk mendapatkan data jika diperlukan.
-  // Contoh:
-  // const data = await fetchDataFunction(params.grief);
-
   return (
     <section className="text-breakWhite pt-28 mb-20 px-10 md:px-24 flex flex-col">
       <div className="w-full text-[24px] font-semibold text-center">
