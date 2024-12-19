6cfcec5a5d72143fce0fa577483e798ba9c5b22c
unknown <dzikri.mr1810@gmail.com>
2024-12-19 10:15:05 +0700

    apala

--- app/catalogue/[grief]/page.tsx
+++ app/catalogue/[grief]/page.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Categories from '@/app/components/categories';
 import ProjectGrief from '@/app/components/projectGrief';
 
-// Fungsi untuk mengambil data dari server
-
-
 // Komponen Grief dengan async di dalamnya
 const Grief = async ({ params }: { params: { grief: string } }) => {
+  // Anda bisa menggunakan fetch di sini untuk mendapatkan data jika diperlukan.
+  // Contoh:
+  // const data = await fetchDataFunction(params.grief);
 
   return (
     <section className="text-breakWhite pt-28 mb-20 px-10 md:px-24 flex flex-col">
@@ -14,7 +14,7 @@ const Grief = async ({ params }: { params: { grief: string } }) => {
         Project <span className="text-lightRed">Categories</span>
       </div>
       <Categories />
-      <ProjectGrief params={params}/>
+      <ProjectGrief params={params} />
     </section>
   );
 };
