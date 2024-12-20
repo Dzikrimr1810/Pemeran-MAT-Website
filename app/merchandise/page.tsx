be94cc1cd3bb67c157d271a59cb06b5a99077882
unknown <dzikri.mr1810@gmail.com>
2024-12-20 14:38:50 +0700

    merch styling

--- app/merchandise/page.tsx
+++ app/merchandise/page.tsx
@@ -13,12 +13,12 @@ export default function Merchandise (){
 
 console.log({merchandiseData})
     return (
-        <section className="md:px-36 px-7 pt-28 overflow-clip md:w-full w-screen">
+        <section className="md:px-36 px-5 pt-28 overflow-clip md:w-full w-screen">
             <div>
                 <div className="w-full text-center mb-10 md:mb-10 justify-center items-center font-bold text-[24px] text-breakWhite">
                     Our <span className="text-lightRed"> Merchandise</span>
                 </div>
-                <div className="grid md:grid-cols-3 gap-5 grid-cols-2 mb-5">
+                <div className="grid md:grid-cols-3 gap-2 md:gap-5 grid-cols-2 mb-5">
                     {merchandiseData.map((items) => (
                         <div key={items.name} className={`${items.name=="Baju Sablon" && "col-start-1"} overflow-hidden md:p-[20px] p-2 border-breakWhite rounded-lg border-[1px]`}>
                             <div className="relative flex justify-center h-4/5 items-end">
@@ -27,7 +27,7 @@ console.log({merchandiseData})
                             </div>
                             <div className="-translate-y-8 md:translate-y-0 font-light text-breakWhite flex flex-col justify-center items-center">
                                 <div className="font-bold text-[18px] md:text-[24px] text-center">{items.name}</div>
-                                <div className="text-center text-[12px] text-gray-500">{items.details}</div>
+                                <div className="text-center text-[10px] md:text-[12px] text-gray-500">{items.details}</div>
                                 <div className="text-lightRed">Rp<span className="text-lightRed text-[18px] md:text-[24px] font-bold">{items.harga}</span>,00</div>
                             </div>
                         </div>
