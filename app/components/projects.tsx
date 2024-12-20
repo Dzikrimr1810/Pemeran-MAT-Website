4a7ddcba77fe481c5459194f3d0f653a3512b55b
unknown <dzikri.mr1810@gmail.com>
2024-12-20 12:42:05 +0700

    catalog revision styling

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -36,15 +36,15 @@ const data: simplifiedCatalogue[] = await getData()
               <Image className='w-full object-center group-hover:scale-105 duration-300' alt={items.name} width={700} height={700} src={items.imageUrl}/>           
             </div>
             <div className='flex flex-col'>
-              <div className='flex text-[9px] md:text-[9px] gap-2 my-1 justify-start items-center'>
-                <div className={`px-4 py-[2px] bg-gray rounded-full flex justify-center items-center
+              <div className='flex text-[9px] md:text-[9px] gap-1 md:gap-2 my-1 justify-start items-center'>
+                <div className={`px-3 md:px-4 py-[2px] bg-gray text-[8pxa] md:text-[9] rounded-full flex justify-center items-center
                   ${items.categoryName == "Denial" && "text-[#B70000]"}
                   ${items.categoryName == "Anger" && "text-[#FF3300]"}
                   ${items.categoryName == "Bargaining" && "text-[#00F2FF]"}
                   ${items.categoryName == "Depression" && "text-[#0054D1]"}
                   ${items.categoryName == "Acceptance" && "text-[#B70000]"}
                   `}>{items.categoryName}</div>
-                <div className='px-4 py-[2px] bg-gray rounded-full flex justify-center items-center'>{items.typeproject}</div>
+                <div className='px-3 md:px-4 py-[2px] bg-gray rounded-full flex justify-center items-center text-[8pxa] md:text-[9]'>{items.typeproject}</div>
               </div>
               <div className='text-[20px] font-semibold pb-2'>{items.name.length > 30 ? items.name.slice(0, 30) + '...' : items.name}</div>
             </div>
